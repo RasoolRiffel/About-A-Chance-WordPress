64a0fe3cced36f43dec916e6af323d0959d7bedc
Your Name <rasooldavids26@gmail.com>
2018-10-08 16:12:31 +0200

    Update

--- functions.php
+++ functions.php
@@ -188,54 +188,17 @@ function be_comment_form_fields( $fields ) {
 }
 add_filter( 'comment_form_default_fields', 'be_comment_form_fields' );
 
-function mytheme_comment($comment, $args, $depth) {
-    if ( 'div' === $args['style'] ) {
-        $tag       = 'div';
-        $add_below = 'comment';
-    } else {
-        $tag       = 'li';
-        $add_below = 'div-comment';
-    }?>
-    <<?php echo $tag; ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ); ?> id="comment-<?php comment_ID() ?>"><?php 
-    if ( 'div' != $args['style'] ) { ?>
-        <div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
-    } ?>
-        <div class="comment-author vcard"><?php 
-            if ( $args['avatar_size'] != 0 ) {
-                echo get_avatar( $comment, $args['avatar_size'] ); 
-            } 
-            printf( __( '<cite class="fn">%s</cite> <span class="says">says:</span>' ), get_comment_author_link() ); ?>
-        </div><?php 
-        if ( $comment->comment_approved == '0' ) { ?>
-            <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em><br/><?php 
-        } ?>
-        <div class="comment-meta commentmetadata">
-            <a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>"><?php
-                /* translators: 1: date, 2: time */
-                printf( 
-                    __('%1$s at %2$s'), 
-                    get_comment_date(),  
-                    get_comment_time() 
-                ); ?>
-            </a><?php 
-            edit_comment_link( __( '(Edit)' ), '  ', '' ); ?>
-        </div>
-
-        <?php comment_text(); ?>
-
-        <div class="reply"><?php 
-                comment_reply_link( 
-                    array_merge( 
-                        $args, 
-                        array( 
-                            'add_below' => $add_below, 
-                            'depth'     => $depth, 
-                            'max_depth' => $args['max_depth'] 
-                        ) 
-                    ) 
-                ); ?>
-        </div><?php 
-    if ( 'div' != $args['style'] ) : ?>
-        </div><?php 
-    endif;
+function has_comments_join( $join, &$wp_query ) {
+    global $wpdb;
+    if ($comment_term = $wp_query->get('post_has_comments')) {
+        $join .= $wpdb->prepare(" JOIN $wpdb->comments ON (($wpdb->posts.ID = $wpdb->comments.comment_post_ID) AND ($wpdb->comments.comment_type = %s) AND ($wpdb->comments.comment_approved = %s)) ", $comment_term['comment_type'], $comment_term['comment_status']);
+    }
+    return $join;
 }
+
+$results = new WP_Query(array(
+    'post_has_comments' => array(
+        'comment_type' => 'review',
+        'comment_status' => '1'
+    )
+));
