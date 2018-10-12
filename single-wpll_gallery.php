28865a17387cb4c619ab8fa77533cc34016564d9
Your Name <rasooldavids26@gmail.com>
2018-10-12 16:35:41 +0200

    Update

---
+++ single-wpll_gallery.php
@@ -0,0 +1,26 @@
+<?php get_header();?>
+
+<div class="container">
+	<?php 
+		if (have_posts()): 
+			while (have_posts()) : the_post();?>
+
+				<h1><?php the_title(); ?></h1>
+
+				<div class="about-txt">
+					<?php the_content(); ?>
+				</div>
+				
+			<?php 
+			endwhile;
+
+			else: 
+				echo "<p>No content found</p>";
+		endif;
+	?>
+</div>
+
+<hr>
+
+
+<?php get_footer();?>
